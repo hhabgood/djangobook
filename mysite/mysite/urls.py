e963b7329047182fbf9b5a5f4bcb8d4bab440e5f
Henry Habgood <henryhabgood@Henrys-MacBook-Pro.local>
2013-01-09 21:32:06 -0500

    Added views.pyc to git

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import hello, homepage
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +7,7 @@ from mysite.views import hello
 
 urlpatterns = patterns('',
 	('^hello/$', hello),
+	('^$', homepage),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
 	return HttpResponse("Hello World")
+
+def homepage(request):
+	return HttpResponse("<b>This is the homepage.</b>")
